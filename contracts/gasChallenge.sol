77127c7d8da797ee05030d0ea49c0d081b58b389
Emmanuel Ifeanyi MECHIE <64092981+emmanuelByte@users.noreply.github.com>
2023-07-12 20:18:19 +0100

    gas optimized

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -2,11 +2,10 @@
 pragma solidity ^0.8.0;
 
 contract gasChallenge {
-    //Implement Fixed-Size Array Technique Here
-    uint[] numbers = [1,2,3,4,5,6,7,8,9,10];
-    
+    // Fixed-Size Array Technique
+    uint[10] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
     //Function to check for sum of array
-    //No changes required in this function
     function getSumOfArray() public view returns (uint256) {
         uint sum = 0;
         for (uint i = 0; i < numbers.length; i++) {
@@ -14,16 +13,23 @@ contract gasChallenge {
         }
         return sum;
     }
-    
+
     function notOptimizedFunction() public {
         for (uint i = 0; i < numbers.length; i++) {
             numbers[i] = 0;
         }
     }
-    
-    //Implement Remaining Gas Optimization Techniques Here
-    //Sum of elements in the numbers array should equal 0
+
     function optimizedFunction() public {
-      
+        // Caching State Variable
+        uint256 numbersLength = numbers.length;
+
+        // Using Unchecked Block
+        unchecked {
+            // For Loop Increment Syntax
+            for (uint i = 0; i < numbersLength; ++i) {
+                numbers[i] = 0;
+            }
+        }
     }
- }
+}
